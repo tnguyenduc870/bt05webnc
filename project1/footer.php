ee15f7cdc57109482c56cc521beb692f293bc94f
tnguyenduc870 <tnguyenduc870@gmail.com>
2022-10-13 16:25:03 +0700

    first commit

---
+++ project1/footer.php
@@ -0,0 +1,18 @@
+<div class="jumbotron">
+     
+<div class="appeal">            
+        </div>
+ 
+<h2>Copyright @ 2020 Design by Duc Thien</h2>
+ 
+</div>
+ 
+</div>
+ 
+<!--/. Container-->
+</div>
+ 
+</div>
+ 
+</body>
+</html>
